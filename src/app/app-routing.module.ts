9995dfc2b81733c68a90c60e73df40c3f616bac0
minhnv20397 <zatuzick03@gmail.com>
2023-05-28 10:50:54 +0700

    dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout/admin-layout.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
 
 const routes: Routes = [
   {
-    path: "admin", component: AdminLayoutComponent,
+    path: "admin", component: AdminLayoutComponent, children: [
+      { path: "", redirectTo: "dashboard", pathMatch: "full" },
+      { path: "dashboard", component: DashboardComponent }
+    ]
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout/admin-layout.component';
-import { BaseLayoutComponent } from './layouts/base-layout/base-layout/base-layout.component';
-import { ProductListComponent } from './components/product-list/product-list/product-list.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HomePageComponent } from './pages/homePage/home-page/home-page.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLayoutComponent,
     BaseLayoutComponent,
-    ProductListComponent
+    ProductListComponent,
+    HomePageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
